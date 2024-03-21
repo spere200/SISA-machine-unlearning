b00496b69a98da20b03da5f1db5b60dd7a426325
Samuel Perez <sperez207@gmail.com>
2024-03-21 14:01:47 -0400

    Created the SISA model For now it just trains each SGDClassifier model on the entire training set and then uses them to vote on a prediction.

---
+++ models/SISA.py
@@ -0,0 +1,54 @@
+from sklearn.linear_model import SGDClassifier
+import numpy as np
+from collections import defaultdict
+
+
+class SISA:
+    def __init__(self, shards=5, slices=5) -> None:
+        """shards: The number of shards and models
+        \nslices: The number of slices and incremental training steps/snapshots created for each model
+        """
+        self.shards = shards
+        self.slices = slices
+
+        # create the weak learning models
+        self.models = [SGDClassifier(loss="log_loss") for _ in range(self.shards)]
+
+    def fit(self, x: np.ndarray, y: np.ndarray) -> None:
+        """x: The independent set of features; expects a 2d numpy array
+        \ny: The dependent features; expects a 1d numpy array"""
+        # for now just train them on the full dataset to test the voting system of self.predict()
+        for model in self.models:
+            model.fit(x, y)
+
+    def predict(self, x: np.ndarray) -> np.ndarray:
+        """x: The dependent feature set on which to make a prediction; expects a 2d numpy array
+        \nReturns a 1d numpy array where the value at i is the prediction for row i in the input
+        """
+        predictions = []
+
+        # go through each row in the input
+        for row in x:
+            currPred = 0
+            finalPred = 0
+            freq = defaultdict(int)
+
+            # have each of the weak learning models vote on the result
+            for model in self.models:
+                currPred = model.predict([row])[0]
+                freq[currPred] += 1
+
+                if freq[currPred] > freq[finalPred]:
+                    finalPred = currPred
+
+            predictions.append(finalPred)
+
+        return np.array(predictions)
+
+    def score(self) -> float:
+        pass
+
+
+if __name__ == "__main__":
+    test = SISA()
+    print(test.models)
